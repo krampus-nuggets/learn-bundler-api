5402b50c1c3e50b534f573f572a5e1c7bd058b38
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-09-27 18:14:37 +0200

    add: app + simple flask api handler

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
+import os
 from flask import send_file, request, Flask
+from modules.pw_microsoft_learn import ms_learn
 
+
+required_folders = {
+    "export": "\\export"
+}
 
 app = Flask(__name__)
-
+working_dir = str(os.path.dirname(os.path.abspath(__file__))) + required_folders["export"]
 
 @app.route("/data")
 def data():
     module = request.args.get("module")
 
     if module:
-        return f"MODULE-URL: {module}"
+        save_location = ms_learn(module, working_dir)
+        return send_file(save_location, mimetype="image/png")
     else:
         return f"ERROR: Invalid Module URL"
-
-"""
-def get_image():
-    if request.args.get('type') == '1':
-       filename = 'ok.gif'
-    else:
-       filename = 'error.gif'
-    return send_file(filename, mimetype='image/gif')
-"""
